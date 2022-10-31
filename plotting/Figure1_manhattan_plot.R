7f38b2982b38889af7a588924490e312c363c14a
yiheng-aug30 <63427663+yiheng-aug30@users.noreply.github.com>
2022-10-30 21:38:10 -0400

    Update Figure1_manhattan_plot.R

--- plotting/Figure1_manhattan_plot.R
+++ plotting/Figure1_manhattan_plot.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(tidyr)
 library(RColorBrewer)
 
+setwd("/home/richards/yiheng.chen/scratch/project1_2_metabolomics_GWAS_CLSA/codes/plotting")
 matabolites_annotation_July2<-read.csv("./CLSA_COMBINED_ANNOTATIONTABLEALL.csv")
 metabo_superpathway_list<-matabolites_annotation_July2 %>% rowwise() %>% mutate(SUPER_PATHWAY_mod=ifelse(SUPER_PATHWAY == "", "Unknown", SUPER_PATHWAY)) %>% select(metabo_ID, SUPER_PATHWAY_mod) %>% distinct()
 
