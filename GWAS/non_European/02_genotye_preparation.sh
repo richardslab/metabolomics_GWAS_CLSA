67dc8f42bb4cdd37ef55bf483f2c58f88375db34
yiheng-aug30 <63427663+yiheng-aug30@users.noreply.github.com>
2022-11-16 13:45:47 -0500

    Update 02_genotye_preparation.sh

--- GWAS/non_European/02_genotye_preparation.sh
+++ GWAS/non_European/02_genotye_preparation.sh
@@ -9,8 +9,8 @@
 ## filter cutoff: maf=0.05; imputation info score: >0.3
 
 pop=south_asian
-work_dir=work_dir=/home/richards/yiheng.chen/scratch/project1_2_metabolomics_GWAS_CLSA/codes/non_EUR_GWAS/non_European_ancestry_GWAS_results
-cd /scratch/richards/yiheng.chen/project1_2_metabolomics_GWAS_CLSA/codes
+work_dir=work_dir=${analysis_dir}/non_EUR_GWAS/non_European_ancestry_GWAS_results
+cd ${analysis_dir}
 
 for i in {1..22}; {
         plink2 --bgen ./data/clsa_imputed_genotype_data/clsa_imp_${i}_v3.bgen ref-first \
